96f163d3557e101ece702f6a29a2ecb3e87b9cc8
dextter1913 <dextter1913@gmail.com>
2021-05-22 15:12:13 -0500

    Agregando formulario de con campo nombre y apellido

--- index.php
+++ index.php
@@ -8,9 +8,28 @@
     <title>deployt</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4"></div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <form action="" method="post">
+                <label for="Nombre">Nombre</label>
+                <input type="text" name="Nombre" id="Nombre" class="form-control">
+                <label for="Apellido">Apellido</label>
+                <input type="text" name="Apellido" id="Apellido" class="form-control">
+                <button type="submit" class="btn btn-outline-warning btn-lg">
+                    Enviar
+                </button>
+            </form>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4"></div>
+    </div>
+</div>
 
 <body>
-<center><h1>Haciendo deployt</h1></center>
+    <center>
+        <h1>Haciendo deployt</h1>
+    </center>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
